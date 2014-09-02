a490ee1c3c8629790cc08ca7b45989802d3063b6
Eric Wolter <eric.wolter@gmx.de>
2014-09-02 14:28:14 +0200

    adds AMatrix debug output

--- fortran/coeffs_and_vels.f90
+++ fortran/coeffs_and_vels.f90
@@ -39,6 +39,12 @@ FUNCTION solve_coeffs(eeps,M,N,XcVecs,tVecs,ExtForce,LQ,pv,wv,LvecMat)
   CPU_p = real(count2-count1)/count_rate
   PRINT *,"Assembling the matrix took ",CPU_p," seconds."
 
+  !!OPEN(10,file="AMat.out");
+  !!DO i=1,3*M*N
+  !!  WRITE(10,'(*(F16.8))') (AMat(i,j),j=1,3*M*N)
+  !!END DO
+  !!CLOSE(10)
+
   CALL SYSTEM_CLOCK(count1, count_rate, count_max)
   !! Assembly of the right hand side for solving Aa=f Eq. (20)
   CALL assemble_rhs(eeps,M,N,XcVecs,tVecs,ExtForce,LQ,pv,wv,LvecMat,Brhs);
